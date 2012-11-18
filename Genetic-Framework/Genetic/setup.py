6fc76e9ac7412f5449ed51c031fb0988dd2c4716
inspectorG4dget <topgunzurhero@gmail.com>
2012-11-18 17:51:22 -0500

    Documentation
    Added documentation

---
+++ Genetic-Framework/Genetic/setup.py
@@ -0,0 +1,34 @@
+'''
+Created on Nov 11, 2012
+
+@author: ashwin
+'''
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+requires = []
+
+setup(
+	name = 'Pyvolution',
+	version = "1.0.1",
+	description = "Evolutionary Algorithms Framework",
+	long_description="Evolutionary Algorithms Framework",
+	author = 'Ashwin Panchapakesan',
+	author_email = 'ashwin.panchapakesan@gmail.com',
+	url = 'https://github.com/inspectorG4dget/Genetic-Framework',
+	packages = ["Genetic"],
+	include_package_data = True,
+	install_requires = requires,
+	license = "Apache License, Version 2.0",
+	zip_safe = False,
+	classifiers = (
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.7',
+	),
+)
